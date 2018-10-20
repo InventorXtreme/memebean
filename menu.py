282f5904f5a5466dec2b8f96619e4390bba457e9
GoBananas2000 <43732918+GoBananas2000@users.noreply.github.com>
2018-10-19 22:48:30 -0400

    Add files via upload

--- menu.py
+++ menu.py
@@ -4,9 +4,9 @@ from tkinter import *
 import tkinter as tk
 import webbrowser
 from tkinter import messagebox
-
+import shoutabout
 ver = memebeanupdate.version
-url = "https://memebean--inventorx.repl.co/"
+url = "https://github.com/InventorXtreme/memebean"
 
 
 # open update page
@@ -15,6 +15,8 @@ def updatelink():
     messagebox.showinfo("Update", "You have version " + ver + "")
 def start():
     memebeanupdate.mempkg()
+def shouta():
+    shoutabout.thing()
 
 main = tk.Tk()
 main.title("Memebean dashboard")
@@ -23,6 +25,8 @@ welcome = tk.Label(main, text="Welcome to Membean")
 welcome.pack(side=TOP)
 launch = Button(main, text="launch memebean", command=start)
 launch.pack()
+shout = Button(main, text="Shoutouts", command=shouta)
+shout.pack()
 updater = Button(main, text="Check for and install updates", command=updatelink)
 updater.pack()
 
